ad6c6d78c64148fedef7e5e46ace4b573fed572d
BlueDev8 <65924485+dfunani@users.noreply.github.com>
2022-09-26 23:03:48 +0200

    Done

--- frontend/src/components/greeting/Greeting.tsx
+++ frontend/src/components/greeting/Greeting.tsx
@@ -9,11 +9,11 @@ export default function Greeting(props: any) {
 			</div>
 			<div className="To-Container">
 				<label htmlFor="ToInput">To:</label>
-				<input type="text" id="ToInput" onInputCapture={(e) => setGreet((prev) => ({...prev, to: e.target.value.toUpperCase() + ', '}))}/>
+				<input type="text" id="ToInput" onInputCapture={(e: any) => setGreet((prev) => ({...prev, to: e.target.value.toUpperCase() + ', '}))}/>
 			</div>
 			<div className="Select-Greeting">
 				<label htmlFor="SelectGreeting">Select Greeting:</label>
-				<select name="Select Greeting" id="SelectGreeting" onChange={(e) => setGreet((prev) => ({...prev, select: e.target.value}))}>
+				<select name="Select Greeting" id="SelectGreeting" onChange={(e: any) => setGreet((prev) => ({...prev, select: e.target.value}))}>
 					<option value="Greeting will go here">--Select greeting--</option>
 					<option value="I choo choo choose you! 🚂">I choo choo choose you! 🚂</option>
 					<option value="You are my sunshine! 🌞">You are my sunshine! 🌞</option>
@@ -22,7 +22,7 @@ export default function Greeting(props: any) {
 			</div>
 			<div className="From-Container">
 				<label htmlFor="FromInput">From:</label>
-				<input type="text" id="FromInput" onInputCapture={(e) => setGreet((prev) => ({...prev, from: 'From: ' + e.target.value.toUpperCase(e)}))}/>
+				<input type="text" id="FromInput" onInputCapture={(e: any) => setGreet((prev) => ({...prev, from: 'From: ' + e.target.value.toUpperCase(e)}))}/>
 			</div>
 		</div>
 	)
